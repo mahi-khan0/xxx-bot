e08afbcfa7e2e004f2f416dff2271e41d8d46ee5
mahi-khan0 <161739658+mahi-khan0@users.noreply.github.com>
2024-03-13 05:04:42 +0600

    Update antibotname.js

--- scripts/events/antibotname.js
+++ scripts/events/antibotname.js
@@ -1,20 +1,28 @@
 module.exports.config = {
-  name: "1",
-  eventType: ["log:user-nickname"],
-  version: "0.0.1",//beta
-  credits: "Nayan",
-  description: "Prevent changing the Bot's nickname"
+	name: "autosetname",
+	eventType: ["log:subscribe"],
+	version: "1.0.3",
+	credits: "D-Jukie",
+	description: "Tự động set biệt danh thành viên mới"
 };
 
-module.exports.run = async function({ api, event, Users, Threads }) {
-    var { logMessageData, threadID, author } = event;
-    var botID = api.getCurrentUserID();
-    var { BOTNAME, ADMINBOT } = global.config;
-    var { nickname } = await Threads.getData(threadID, botID);
-    var nickname = nickname ? nickname : BOTNAME;
-    if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-   api.changeNickname(`『 ${global.config.PREFIX } 』• ${(!global.config.BOTNAME) ? "BOT" : global.config.BOTNAME}`, threadID, botID) 
-        var info = await Users.getData(author);
-       return api.sendMessage({ body: `[WARN] - ${info.name} - bot change protection is enabled`}, threadID);
-    }  
-  }
+module.exports.run = async function({ api, event, Users }) {
+const { threadID } = event;
+var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId)
+	for (let idUser of memJoin) {
+		const { readFileSync, writeFileSync } = global.nodemodule["fs-extra"];
+		const { join } = global.nodemodule["path"]
+		const pathData = join("./modules/commands","cache", "autosetname.json");
+		var dataJson = JSON.parse(readFileSync(pathData, "utf-8"));
+		var thisThread = dataJson.find(item => item.threadID == threadID) || { threadID, nameUser: [] };
+		if (thisThread.nameUser.length == 0) return 
+		if (thisThread.nameUser.length != 0) {  
+		var setName = thisThread.nameUser[0] 
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		var namee1 = await api.getUserInfo(idUser)
+        var namee = namee1[idUser].name
+		api.changeNickname(`${setName} ${namee}`, threadID, idUser);
+		} 
+	}	
+	return api.sendMessage(`Đã set biệt danh tạm thời cho thành viên mới`, threadID, event.messageID)
+                          }
